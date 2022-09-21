1b1e4a396a1cdffa5e43c537c58d335d2f08d908
Mikael Voss <mvs@nyantec.com>
2022-09-21 18:33:47 +0200

    akkoma: init at 3.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1125,6 +1125,8 @@ with pkgs;
 
   adminer = callPackage ../servers/adminer { };
 
+  akkoma = callPackage ../servers/akkoma { };
+
   advancecomp = callPackage ../tools/compression/advancecomp {};
 
   aefs = callPackage ../tools/filesystems/aefs { };
